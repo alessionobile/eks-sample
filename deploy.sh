e3a5d565675c89e4d41c1b3050ebcc028c13ed28
Alessio Nobile <netziro@gmail.com>
2025-04-29 11:52:41 +0200

    fix: deploy/destroy scripts

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,26 @@
 source vars.sh
 
 ## Install terraform
+echo -e "$(prefix)$YELLOW [Dependencies] Install Terraform.$ENDCOLOR"
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
 ## Install helm
+echo -e "$(prefix)$YELLOW [Dependencies] Install Helm.$ENDCOLOR"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 source get_helm.sh
 
 cd src/terraform/
-echo -e "$(prefix)$YELLOW [Terraform] Initializing the project.$ENDCOLOR\n"
+
+## Initialize terraform
+echo -e "$(prefix)$YELLOW [Terraform] Initializing the project.$ENDCOLOR"
 terraform init -input=false
 echo -e "$(prefix)$GREEN [Terraform] Project initialized.$ENDCOLOR\n"
 
+## Deploy project
 echo -e "$(prefix)$YELLOW [Terraform] Deploying the project.$ENDCOLOR"
 echo -e "$(prefix)$CYAN INFO: ☕ Go grab a coffee while we'll wait for Terraform to deploy the project. It will take about 15 minutes.$ENDCOLOR\n"
-terraform apply -input=false
-echo -e "$(prefix)$GREEN [Terraform] ✅ Deployment completed.$ENDCOLOR\n"
+terraform apply -input=false -auto-approve
+echo -e "$(prefix)$GREEN [Terraform] ✅ Deployment completed.$ENDCOLOR"
--- destroy.sh
+++ destroy.sh
@@ -7,7 +7,8 @@ source vars.sh
 
 cd src/terraform/
 
+## Destroys the project
 echo -e "$(prefix)$YELLOW [Terraform] Destroying the project.$ENDCOLOR"
 echo -e "$(prefix)$CYAN INFO: ☕ Go grab (another) coffee while we'll wait for Terraform to deploy the project. It will take about 15 minutes.$ENDCOLOR\n"
-terraform destroy -input=false
-echo -e "$(prefix)$GREEN [Terraform] ✅ Deployment completed.$ENDCOLOR\n"
+terraform destroy -input=false -auto-approve
+echo -e "$(prefix)$GREEN [Terraform] ✅ The project has been destroyed.$ENDCOLOR\n"
